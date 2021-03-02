4ffd82eadb0820ed673130e9933aa57d3a325ce0
gary@MacbookAir <>
2021-03-02 10:31:47 +0800

    update

--- sc_dining_rewrite.js
+++ sc_dining_rewrite.js
@@ -2,12 +2,12 @@ var fs = require('fs-extra');
 var parser = require('xml2json');
 const readXlsxFile = require('read-excel-file/node');
 
-var sourceFolder = __dirname + "\\dining_content\\source\\dining_site_content_repack";
-var distFolder = __dirname + "\\dining_content\\dist\\dining_site_content_repack";
-var translationSource = __dirname + "\\dining_content\\translation.xlsx";
+var sourceFolder = __dirname + "\/dining_content\/source\/sc-dining-content";
+var distFolder = __dirname + "\/dining_content\/dist\/sc-dining-content";
+var translationSource = __dirname + "\/dining_content\/translation.xlsx";
 
 fs.copySync(sourceFolder,distFolder)
-var folderName = "\\jcr_root\\content\\am-content\\sc\\earn-partner\\dining";
+var folderName = "\/jcr_root\/content\/am-content\/sc\/earn-partner\/dining";
 var files = fs.readdirSync(distFolder + folderName);
 
 var readingExcel = new Promise((resolve, reject) =>{
@@ -16,18 +16,18 @@ var readingExcel = new Promise((resolve, reject) =>{
   });
 })
 var translationData = {};
+var errorLog = [];
 
 readingExcel.then((rows) => {
   for (var i = 0; i < rows.length; i++) {
     var scLink = rows[i][5];
-    if(scLink != null && typeof scLink.split('/')[7] != 'undefined') {
+    if(scLink != null && typeof scLink.split('/')[7] != 'undefined' && rows[i][6] != null) {
       translationData[scLink.split('/')[7]] = {
-        description: rows[i][6],
+        description: rows[i][6].replace(/\r\n/g,'<br />'),
+        tnc: rows[i][9] != null ? rows[i][9].replace('条款及细则\r\n','').replace('条款及细则 \r\n','').replace(/\r\n/g,'<br />') : null,
       };
     }
   }
-
-  console.log(translationData);
 
   for (var i = 0; i < files.length; i++) {
     //brand level
@@ -36,12 +36,34 @@ readingExcel.then((rows) => {
       continue;
     }
 
-    var brandFilePath = distFolder + folderName + "\\" + brandKey + "\\.content.xml";
+    var brandFilePath = distFolder + folderName + "\/" + brandKey + "\/.content.xml";
     var brandFile = fs.readFileSync(brandFilePath).toString();
     var brandJson = parser.toJson(brandFile);
-    die();
-    console.log(brandJson);
-    // die()
+    var brandObj = JSON.parse(brandJson);
+    // die();
+
+    var translationBrand = translationData[brandKey];
+
+    if(typeof translationBrand != "undefined") {
+      brandObj['jcr:root']['jcr:content']['par']['brands_admin']['brand-description'] = translationBrand.description;
+      brandObj['jcr:root']['jcr:content']['par']['brands_admin']['terms-and-conditions-details'] = translationBrand.tnc;
+    } else {
+      errorLog.push("key: " + brandKey + " not found in translation");
+    }
+
+    brandJson = JSON.stringify(brandObj);
+
+    var brandXml = parser.toXml(brandJson,{sanitize:true});
+    brandXml = brandXml.replace(/&gt;/g,">");
+    brandXml = brandXml.replace(/\n/g,"&#xa;");
+
+    fs.writeFileSync(brandFilePath,brandXml);
+    console.log("writing: " + brandFilePath);
+  }
+
+  console.log("There are " + errorLog.length + " error found during the execution");
+  for (var i = 0; i < errorLog.length; i++) {
+    console.log(errorLog[i]);
   }
   // fs.readdirSync(distFolder,(err,files) => {
   //   files.forEach(file=> {
